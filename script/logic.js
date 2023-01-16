7189a1f323ffbd30dd4a0e3b691339bec1b91cc7
Jack <116763735+GitJackL@users.noreply.github.com>
2023-01-16 11:55:51 +0000

    Added countdown function for quiz timer and global variables 'time' and 'intervalID'

--- script/logic.js
+++ script/logic.js
@@ -10,3 +10,15 @@ function startQuiz() {
   document.getElementById("questions").classList.remove("hide");''
 }
 
+
+function countdown() {
+  time--;
+  document.getElementById("time").innerHTML = time;
+  if (time === 0) {
+      clearInterval(intervalId);
+      console.log( "You are out of time")
+  }
+}
+
+var time = 60;
+var intervalId;
